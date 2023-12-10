484c4f5e5d5b4f4015aa01018602f02c0b58b808
AliRaza <ali.raza@etaplus.energy>
2023-12-10 21:48:04 +0100

    Testing

---
+++ cypress.config.ts
@@ -0,0 +1,23 @@
+import { defineConfig } from "cypress";
+
+
+export default defineConfig({
+  video:true,
+  e2e: {
+    
+    reporter:'cypress-mochawesome-reporter',
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    require('cypress-mochawesome-reporter/plugin')(on);
+   
+      
+    
+      
+  },
+  defaultCommandTimeout: 3000,
+  
+  },
+  
+ 
+  
+})
---
+++ cypress/e2e/Main page.cy.ts
@@ -0,0 +1,6 @@
+///<refernce types="cypress"/>
+
+it('first test',() =>{
+ cy.visit("https://www.saucedemo.com/")
+ cy.screenshot();
+})
